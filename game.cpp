55639ed3e7e2913106dec23b0ddefecc5720a400
Isaac Satz <isaacsatz@gmail.com>
2014-06-04 14:50:52 -0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <movement.cpp>
 
 using namespace std;
+int x [5] = {1,2,2,2,2,3};
+int y [5] = {1,1,2,2,2,3};
 
 
-
+char char1 = "a";
 int main(){
-
+  cin<<char1<<endl;
+  if(char1="a"){
+    moveLeft(x);
+  }
 }
