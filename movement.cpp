971b4ca62e200495de6ce12180ad5e8b7636b10c
Isaac Satz <isaacsatz@gmail.com>
2014-06-04 15:04:50 -0700

    Update movement.cpp

--- movement.cpp
+++ movement.cpp
@@ -2,17 +2,11 @@
 using namespace std;
 
 moveLeft(){
-  for(int i=0; i<5; i++){
-    x[i]=x[i-1];
-  }
+ 
 }
 moveRight(){
-  for(int i=0; i<5; i++){
-    x[i]=x[i+1];
-  }
+  
 }
 jump(){
-  for(int i=0; i<5; i++){
-    y[i]=y[i-1];
-  }
+
 }
