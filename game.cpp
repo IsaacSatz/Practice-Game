906d7d1e12a9926eb1dbf01d62eb988eb9ea85cc
Isaac Satz <isaacsatz@gmail.com>
2014-06-04 15:19:32 -0700

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,15 +3,25 @@
 
 using namespace std;
 
+bool isPlaying = true;
+String name = "name";
 char character = "O";
 char char1 = "a";
 int main(){
-  cin<<char1<<endl;
-  if(char1="a"){
-    x.moveLeft;
-  }else if(char1="d"){
-    x.moveRight();
-  }else if(char1="w"){
-    x.jump();
+
+  cout>>"Enter your name to start">>endl;
+  cin<<name<<endl;
+  cout>>"Game has begun, use WASD to move">>endl;
+  while(isPlaying){
+    cin<<char1<<endl;
+      if(char1="a"){
+         x.moveLeft;
+     }else if(char1="d"){
+         x.moveRight();
+    }else if(char1="w"){
+        x.moveUp();
+    }else if(char1="s"){
+        x.moveDown();
+    } 
   }
 }
